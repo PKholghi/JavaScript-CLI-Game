efcf55f43a375211a33c52560b78bb947d357d34
Ben <benlm1@hotmail.com>
2021-03-04 15:14:05 +0000

    Added random attack to monsterClass

--- monsterClass.js
+++ monsterClass.js
@@ -15,22 +15,18 @@ class monster{
     }   
 
     defeat(){
-      return 'You have defeated the '+this.name+'. Well done!'
+      return 'You have defeated the '+this.name+'. Well done!';
     }
 
     attack(){
-      return _.sample(this.attacks);
+        return this.attacks[Math.floor(Math.random()*this.attacks.length)]; 
     }
 }
 
 
-//Goblin example
+/*Goblin example
 var Goblin=new monster('Goblin',30,'100 gold');
-console.log(Goblin);
-
 Goblin.addAttack('The goblin punches you',20);
 Goblin.addAttack('The goblin kicks you',30)
-Goblin.addAttack('The goblin bites you',25)
-console.log(Goblin);
-
-
+Goblin.addAttack('The goblin bites you',25) 
+*/ 

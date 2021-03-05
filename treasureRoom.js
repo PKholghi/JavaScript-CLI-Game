85816a81172f96f9a15b46590e00fd94379ba22a
Ben <benlm1@hotmail.com>
2021-03-05 14:40:25 +0000

    fixed treasureRoom conflict

--- treasureRoom.js
+++ treasureRoom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class treasureRoom{
     constructor(){
         this.message='You enter a large room. Upon a pedestal against the furthest wall, an ornate chest glints.'
@@ -8,6 +7,3 @@ class treasureRoom{
         return 'Congratulations, you found the treasure and beat the game!!! You earned '+playerPoints+' points and defeated '+playerKills+' monters.';
     }
 }
-=======
-class treasure {}
->>>>>>> b1b065393a28e63ad20237eba5ef492fa2e3d98f

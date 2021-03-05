4bb65077ad3e4a057befdd901b8ec2c460bf234c
Ben <benlm1@hotmail.com>
2021-03-05 11:36:37 +0000

    added combat systemt to monsterClass

--- monsterClass.js
+++ monsterClass.js
@@ -1,13 +1,12 @@
+import promptSync from 'prompt-sync';
+let prompt = promptSync({sigint: true});
+
 class monster{
-    constructor(name,health,reward) {
+    constructor(name,health,points) {
         this.name=name;
         this.health=health;
-        this.reward=reward;
+        this.points=points;
         this.attacks=[];
-    }
-
-    takesDamage(damage){
-        this.health-=damage;
     }
 
     addAttack(description,damage){
@@ -21,12 +20,45 @@ class monster{
     attack(){
         return this.attacks[Math.floor(Math.random()*this.attacks.length)]; 
     }
+
+    fight(){
+        let action=prompt(`A ${this.name} attacks you! What will you do? (fight/run away) `);
+        while (true){
+            if (action=='fight'){
+                this.health-=heroDamage;
+                console.log(`\nYou hit the ${this.name} and deal ${heroDamage} damage. The ${this.name} now has ${this.health} health.`); //need to link to character 
+            } else if (action=='run away'){
+                console.log('\nYou can\'t run away!'); //Go back to previous room??
+            } else {
+                console.log('\nYou can\'t do that!');
+            }
+            if (this.health<=0){
+                console.log(`\nYou defeated the ${this.name}!`);
+                break
+            }
+            let monsterAttack=this.attack();
+            heroHealth-=monsterAttack[1]; //need to link to character
+            console.log(monsterAttack[0]+` You now have ${heroHealth} health.`); //need to link to character
+            
+            if (heroHealth<=0){ //need to link to character
+                console.log('\nYou have been defeated. Game over.');
+                break;
+            }
+
+            action=prompt('What will you do? (fight/run away) ')
+        }
+    }
 }
 
 
-/*Goblin example
-var Goblin=new monster('Goblin',30,'100 gold');
+/*Goblin Example
+var Goblin=new monster('Goblin',30,100);
 Goblin.addAttack('The goblin punches you',20);
 Goblin.addAttack('The goblin kicks you',30)
 Goblin.addAttack('The goblin bites you',25) 
-*/ 
+
+var heroDamage=2;
+var heroHealth=100;
+
+Goblin.fight();
+*/

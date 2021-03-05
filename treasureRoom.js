3e04092ea3972647540cc1c25208e61984350025
Ben <benlm1@hotmail.com>
2021-03-05 14:09:16 +0000

    treasureRoom

--- treasureRoom.js
+++ treasureRoom.js
@@ -1,1 +1,9 @@
-class treasure
+class treasureRoom{
+    constructor(){
+        this.message='You enter a large room. Upon a pedestal against the furthest wall, an ornate chest glints.'
+    }
+
+    openChest(playerPoints,playerKills){
+        return 'Congratulations, you found the treasure and beat the game!!! You earned '+playerPoints+' points and defeated '+playerKills+' monters.';
+    }
+}
